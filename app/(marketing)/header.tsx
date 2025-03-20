81db3e8a28d06cecb3309eeec87778d2c254c4af
xiloong <bright_star@foxmail.com>
2025-03-20 12:20:59 +0800

    Temporarily complete Authentication

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -1,7 +1,42 @@
+import { Button } from "@/components/ui/button"
+import { ClerkLoaded, ClerkLoading, SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs"
+import { Loader } from "lucide-react"
+import Image from "next/image"
+
 export const Header = () => {
-    return (
-        <header className="h-20 w-full border-b-2 border-slate-200 px-4">
-            Header
-        </header>
-    )
+  return (
+    <header className="h-20 w-full border-b-2 border-slate-200 px-4">
+      <div className="lg:max-w-screen-lg mx-auto flex items-center justify-between h-full ">
+        <div className="flex items-center  pl-3 gap-x-3">
+          <Image src="/duolingoicon.svg" height={40} width={40} alt="DuolingoIcon" />
+          <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">
+            Lingo
+          </h1>
+        </div>
+
+        <ClerkLoading>
+          {/* 加载中 */}
+          <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+        </ClerkLoading>
+
+        <ClerkLoaded>
+          <SignedIn>
+            <UserButton afterSignOutUrl="/" />
+          </SignedIn>
+          <SignedOut>
+            <SignInButton
+              mode="modal"
+              forceRedirectUrl="/learn"
+            // 此处可能要进行修改
+            >
+              <Button size='lg' variant='ghost'>
+                Login
+              </Button>
+            </SignInButton>
+          </SignedOut>
+        </ClerkLoaded>
+
+      </div>
+    </header>
+  )
 }
