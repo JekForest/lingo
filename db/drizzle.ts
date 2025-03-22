1b388693c09fe16a06b3b09af21ced860b9a60b5
xiloong <bright_star@foxmail.com>
2025-03-22 11:15:45 +0800

    Complete Schema

--- db/drizzle.ts
+++ db/drizzle.ts
@@ -4,7 +4,8 @@ import { drizzle } from 'drizzle-orm/neon-http'
 import * as schema from "./schema"
 
 const sql = neon(process.env.DATABASE_URL!)
-const db = drizzle({ client: sql, schema })
+// const db = drizzle({ client: sql, schema })
+const db = drizzle(sql, { schema })
 //可能有问题
 
 export default db
